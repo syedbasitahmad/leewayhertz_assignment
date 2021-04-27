951c440027bc756d446eb16b8253308ba44f366f
BasitAhmad <basitahmad4@gmail.com>
2021-04-27 21:40:19 +0530

    Init commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var express=require('express');
+var app=express();
+var bodyparser=require('body-parser');
+var mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/book');
+var db=mongoose.connection;
+Book =require('./models/book');
+
+app.get('/api/books', (req, res) => {
+	Book.getBooks((err, books) => {
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
+});
+
+app.get('/api/books/:_id', (req, res) => {
+	Book.getBookById(req.params._id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.post('/api/books', (req, res) => {
+	var book = req.body;
+	Book.addBook(book, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.put('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	var book = req.body;
+	Book.updateBook(id, book, {}, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.listen(3000);
+console.log("running on port 3000...")
